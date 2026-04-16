48dc5f61b1ab8ecb61843efaa5cd846e4497ecf8
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-16 15:08:47 +0700

    menambahkan banyaknya array dan elemen array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,3 +6,31 @@ int nPanjang;
 int x;
 
 void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya arrayn(maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+       
+    }
+
+    cout << "\n====================================================\n";
+    cout << "             Masukkan elemen array                    \n";
+    cout << "======================================================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+
+}
